ed42033b331f309f25ff9fd94e5f55e23114f961
emre-worq-company <emre.kabaca@worqcompany.com>
2024-06-05 12:26:35 +0300

    redux store problem fixed

--- src/app/providers.js
+++ src/app/providers.js
@@ -5,7 +5,14 @@ import { Provider } from 'react-redux'
 import { makeStore } from '@/redux/store'
 import { PrimeReactProvider } from 'primereact/api';
 
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
+import { errorInterceptor } from '@/app/errorInterceptor';
+
 export default function StoreProvider({ children }) {
+  errorInterceptor();
+
   const storeRef = useRef()
   if (!storeRef.current) {
     // Create the store instance the first time this renders
